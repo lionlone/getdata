251fc5ead35a7dee537a3b3bb0b0aa34d46d5114
lionlone <botxabong009@gmail.com>
2016-04-19 13:05:46 +0700

    lionlone
    lionlone

---
+++ chodientu/get.php
@@ -0,0 +1,59 @@
+<?php 
+include('simple_html_dom.php');
+//error_reporting(0);
+// khai bai biến071011007100710072014
+$out_link_shop_unique = array();
+$out_info_shop_refine = array();
+$link_shop = "";
+//
+//reg 
+$preg_link_shop = '/<a class="p-seller" target="_blank" href="(.*)">(.*)<\/a>/U';
+$preg_info_shop = '/<li><i (.*)"><\/i>(.*)<\/li>/U';
+//=====
+$url = "https://www.chodientu.vn/dien-thoai-di-dong";
+$url_page[] = $url;
+for ($i=2; $i <= 7; $i++) {
+	$url_page[] = "$url?trang=$i";
+	foreach ($url_page as $key => $value) {
+		$content = file_get_contents($value);
+		$link_shop = $link_shop . $content;
+	}
+}
+preg_match_all($preg_link_shop, $link_shop, $out_link_shop, PREG_SET_ORDER);
+//loai bo shop trùng nhau trong mang $out_link_shop
+foreach ($out_link_shop as $value) {
+	$link_shop_unique[] = $value[1];
+}
+$out_link_shop_unique = array_unique($link_shop_unique);
+// lấy thoogn tin shop
+
+foreach ($out_link_shop_unique as $key => $value) {
+	$out_info_shop = '';
+	$info_refine = array();
+	$info_shop = file_get_contents($value);
+	preg_match_all($preg_info_shop, $info_shop , $out_info_shop, PREG_SET_ORDER);
+	// xóa các phần tử trong mảng
+	$count_info_refine = count($info_refine);
+	for ($i=0; $i < $count_info_refine; $i++) { 
+		array_shift($info_refine);
+	}
+	foreach ($out_info_shop as $key => $valuee) {
+		$info_refine[] = $valuee[2];
+	}
+	$info_refine[] = $value;
+	$out_info_shop_refine[] = $info_refine;
+}
+echo '<pre>';
+var_dump($out_info_shop_refine);
+
+
+
+/*preg_match_all('/<li><i (.*)"><\/i>(.*)<\/li>/U',
+    file_get_contents($value[0][1]),
+    $out_contact, PREG_SET_ORDER);
+foreach ($out_contact as $key => $value) {
+	echo $value[2] . '<br />';
+}*/
+?>
+
+
